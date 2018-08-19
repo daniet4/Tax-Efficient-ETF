c0e177dbbfa114a4c312634350a9c8eb30c01f11
daniet4 <fatboar@gmail.com>
2018-08-18 23:35:10 -0700

    Add files via upload

--- parseStocks.py
+++ parseStocks.py
@@ -4,7 +4,16 @@
 # For tax efficiency, the remaining high dividend yielding stocks will be held in a tax advantaged account such as a
 # 401k, IRA, or HSA.
 
-# Updated: 8/11/18
+# Updated: 8/15/18
+
+
+import os
+import glob
+
+from bs4 import BeautifulSoup
+import pandas as pd
+from xlsxwriter.utility import xl_rowcol_to_cell
+import win32com.client as win32
 
 """
 0. Grab Fidelity Stock Screener data or something similar.
@@ -19,14 +28,6 @@
     e.g. all stocks that have an optimal holding of less than 0.1%.
 """
 
-import os
-import glob
-
-from bs4 import BeautifulSoup
-import pandas as pd
-# from xlsxwriter.utility import xl_rowcol_to_cell
-
-
 class Stocks(object):
     def __init__(self, path, div):
         # Initializations
@@ -39,12 +40,6 @@ class Stocks(object):
         # Loading data
         self.loadXLSData()
 
-        # Removing extraneous columns
-        self.deleteData()
-
-        # Convert abbreviations to integers
-        self.str2int()
-
         # Sort companies by dividend yield
         self.sortDividendYield()
 
@@ -55,7 +50,7 @@ class Stocks(object):
         self.writeStocks()
 
         # Return a stock investment Excel file
-        self.getGuide(div)
+        self.makeGuide(div)
 
 
     def grabXLSData(self):
@@ -64,6 +59,9 @@ class Stocks(object):
     def loadXLSData(self):
         """Read in data from an Excel spreadsheet"""
         self.data = pd.read_excel(self.path)
+        self.data[self.colDY] = self.data[self.colDY].fillna(0) / 100
+        self.deleteData()
+        self.str2int()
 
     def deleteData(self):
         uselessCols = ['S&P 500 (R)', 'Security Type']
@@ -99,51 +97,67 @@ class Stocks(object):
             raise NameError(msg)
 
     def writeStocks(self):
-        ## Write to test.csv
-        # self.data.to_csv('test.csv')
-
-        # Write to test.xlsx
         try:
-            writer = pd.ExcelWriter('test.xlsx')# engine='xlsxwriter')
+            fName = 'test.xlsx'
+            writer = pd.ExcelWriter(fName)# engine='xlsxwriter')
             self.data.to_excel(writer, 'Sheet1')
             writer.save()
+            self.formatExcel(fName)
         except PermissionError:
-            msg = 'Please close {}. The program is trying to write to it.'.format('test.xlsx')
+            msg = 'Please close {}. The program is trying to write to it.'.format(fName)
             raise PermissionError(msg)
 
     def writeGuide(self):
-        ## Write to guide.csv
-        # self.guide.to_csv('guide.csv')
-
         # Write to guide.xlsx
         try:
-            writer = pd.ExcelWriter('guide.xlsx')#, engine='xlsxwriter')
-            self.guide.to_excel(writer, 'Sheet1')
+            fName = 'guide.xlsx'
+            writer = pd.ExcelWriter(fName)#, engine='xlsxwriter')
+            cols = ['Symbol', 'Company Name', 'Optimal Holding %', 'Dividend Yield', 'Price Performance (52 Weeks)',
+                    'Weighted Market Capitalization', 'Market Capitalization', 'Security Price']
+            self.guide[cols].to_excel(writer, 'Sheet1')
             workbook = writer.book
             worksheet = writer.sheets['Sheet1']
+
+            # Formats
+            centsFormat = workbook.add_format({'num_format': '$0.00'})
+            dollarsFormat = workbook.add_format({'num_format': '$#,###,###,###,###'})
+            percentFormat = workbook.add_format({'num_format': '0.00%'})
+
+            # Formatting
+            worksheet.set_column('D:G', 1, percentFormat)
+            worksheet.set_column('H:H', 1, dollarsFormat)
+            worksheet.set_column('I:I', 1, centsFormat)
+
             writer.save()
         except IOError:
-            msg = 'Please close {}. The program is trying to write to it.'.format('test.xlsx')
+            msg = 'Please close {}. The program is trying to write to it.'.format(fName)
             raise IOError(msg)
+        return fName
 
-    def getGuide(self, div):
+    def formatExcel(self, fName):
+        excel = win32.gencache.EnsureDispatch('Excel.Application')
+        wb = excel.Workbooks.Open(os.path.join(os.path.dirname(self.path), fName))
+        ws = wb.Worksheets('Sheet1')
+        ws.Columns.AutoFit()
+        wb.Save()
+        excel.Application.Quit()
+
+    def makeGuide(self, div):
         self.guide = self.data[self.data[self.colDY] < div]
         weightedMarketCapLowDiv = self.guide['Weighted Market Capitalization'].sum()
-        self.guide['Optimal Holding %'] = self.guide['Weighted Market Capitalization'] / weightedMarketCapLowDiv * 100
+        self.guide['Optimal Holding %'] = self.guide['Weighted Market Capitalization'] / weightedMarketCapLowDiv
         self.guide['Average Dividend Yield %'] = (self.guide['Optimal Holding %'] * self.guide[self.colDY]).sum() / 100
-        self.writeGuide()
+        fName = self.writeGuide()
+        self.formatExcel(fName)
 
 def main():
     # Initialization
     pd.set_option('display.expand_frame_repr', False)
-
-    path = r"E:\Daniel\Downloads"
-    path= r"/home/daniet4/"
-    fName = r"sp500 data.xls"
-    fName = glob.glob('*.xls')[0]
-
+    path = r"C:\Users\Daniel\Desktop\Python Scripts\ETF"
+    # fName = glob.glob('sp.xls')[0]
+    fName = 'sp500_data.xlsx'
     fullPath = os.path.join(path, fName)
-    div = 0.1
+    div = 1
 
     stocks = Stocks(fullPath, div)
     return stocks
@@ -151,4 +165,4 @@ def main():
 
 if __name__ == '__main__':
     SP500 = main()
-    stop = 1
+
